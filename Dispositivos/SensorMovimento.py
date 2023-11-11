49551b941a01df1fca6d1787c5343053ab54aa70
Lucas Martins de Barros <00038997@randon.com.br>
2023-11-11 13:34:40 -0300

    [feat] documentada classe SensorMovimento

--- Dispositivos/SensorMovimento.py
+++ Dispositivos/SensorMovimento.py
@@ -4,10 +4,34 @@ from random import randint
 
 class SensorMovimento(Dispositivo):
     """
+    Simula um sensor de movimento.
+
+    Herda da classe Dispositivo e representa um sensor de movimento que gera
+    dados simulados de movimentação com base em horários e probabilidades específicas.
+
+    Atributos
+    ---------
+        - horarioInicial (datetime.time): O horário inicial para considerar a possibilidade de movimento.
+            Padrão = 19:00 (7:00 PM).
+        - diferencaTempoFinal (timedelta): O intervalo de tempo durante o qual o movimento é possível.
+            Padrão = 2 horas.
+        - chanceMovimento (int): A probabilidade de detectar movimento.
+            Padrão = 5%.
     """
 
-    def __init__(self, token : str, horarioInicial: datetime.time = time(19, 00), diferencaTempoFinal: timedelta = timedelta(hours=2), chanceMovimento : int = 5) -> None:
+    def __init__(self, token: str, horarioInicial: datetime.time = time(19, 00), diferencaTempoFinal: timedelta = timedelta(hours=2), chanceMovimento: int = 5) -> None:
         """
+        Inicializador da classe do sensor de movimento.
+
+        Parâmetros
+        ----------
+            - token (str): Token do dispositivo no TagoIO
+            - horarioInicial (datetime.time): O horário inicial que define se vai enviar ou não o movimento.
+                Padrão = 19:00 (7:00 PM).
+            - diferencaTempoFinal (timedelta): O intervalo de tempo par adefinir o horário final para considerar ou não o movimento.
+                Padrão = 2 horas.
+            - chanceMovimento (int): A probabilidade de detectar movimento.
+                Padrão = 5%.
         """
         self.horarioInicial = horarioInicial
         self.diferencaTempoFinal = diferencaTempoFinal
@@ -16,6 +40,12 @@ class SensorMovimento(Dispositivo):
     
     def geraDados(self) -> dict:
         """
+        Gera dados simulados de movimento.
+
+        Retorna
+        -------
+            dict: Um dicionário contendo informações sobre a detecção de movimento, incluindo
+                'variable', 'value' e 'time'. Retorna None se não houver movimento detectado.
         """
         chance = randint(0, 100)
         if chance <= self.chanceMovimento and self.validaHorario():
@@ -27,7 +57,14 @@ class SensorMovimento(Dispositivo):
         else:
             return None
     
-    def validaHorario(self):
+    def validaHorario(self) -> bool:
+        """
+        Verifica se o horário atual está dentro do intervalo de tempo definido para detectar movimento.
+
+        Retorna
+        -------
+            bool: True se estiver dentro do horário definido, False caso contrário.
+        """
         horarioAgora = datetime.now()
         horarioInicialHoje = datetime.combine(horarioAgora.date(), self.horarioInicial)
         horarioFinalHoje = horarioInicialHoje + self.diferencaTempoFinal
