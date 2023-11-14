cac7bac0d11c417430ac37aca9016b0335b6f0b4
Lucas Martins de Barros <00038997@randon.com.br>
2023-11-14 18:07:26 -0300

    [feat] adicionado parâmetro geral de quantidadeExecucoes

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from multiprocessing import Process
 import datetime
 import time
 
+quantidadeExecucoes = 100
+
 def geraDadosTermometro(termometro : Termometro, intervalo : int = 30):
-    for _ in range(10):
+    for _ in range(quantidadeExecucoes):
         time.sleep(intervalo)
         try:
             dados = termometro.geraDados()
@@ -17,7 +19,7 @@ def geraDadosTermometro(termometro : Termometro, intervalo : int = 30):
             print(e)
             
 def geraDadosSensorAgua(sensorAgua : SensorAgua, intervalo : int = 30):
-    for _ in range(10):
+    for _ in range(quantidadeExecucoes):
         time.sleep(intervalo)
         try:
             dados = sensorAgua.geraDados()
@@ -27,7 +29,7 @@ def geraDadosSensorAgua(sensorAgua : SensorAgua, intervalo : int = 30):
             print(e)
 
 def geraDadosSensorUmidade(sensorUmidade : SensorUmidade, intervalo : int = 30):
-    for _ in range(10):
+    for _ in range(quantidadeExecucoes):
         time.sleep(intervalo)
         try:
             dados = sensorUmidade.geraDados()
@@ -37,7 +39,7 @@ def geraDadosSensorUmidade(sensorUmidade : SensorUmidade, intervalo : int = 30):
             print(e)
 
 def geraDadosSensorMovimento(sensorMovimento : SensorMovimento, intervalo : int = 15):
-    for _ in range(10):
+    for _ in range(quantidadeExecucoes):
         time.sleep(intervalo)
         try:
             dados = sensorMovimento.geraDados()
@@ -49,7 +51,7 @@ def geraDadosSensorMovimento(sensorMovimento : SensorMovimento, intervalo : int 
 
 if __name__ == '__main__':
     termometro = Termometro(token="e5c937f2-164b-46a1-958c-a2eb2171d75c")
-    sensorMovimento = SensorMovimento(token="25f8b7b9-0ef4-453d-bdf0-ace0559b7033", horarioInicial=datetime.time(16,00),chanceMovimento=100)
+    sensorMovimento = SensorMovimento(token="25f8b7b9-0ef4-453d-bdf0-ace0559b7033", horarioInicial=datetime.time(16,00),chanceMovimento=30)
     sensorAgua = SensorAgua(token="719af90e-03ac-4157-9cde-2cd3a05df332")
     sensorUmidade = SensorUmidade(token="0a03cbcc-e913-4482-b996-d129a39844bb")
     
